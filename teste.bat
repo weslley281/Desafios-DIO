caab0ab57c37aa4e13db770695eaa8474feef902
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2024-08-22 15:46:16 -0400

    a

---
+++ teste.bat
@@ -0,0 +1,7 @@
+sudo setoolkit
+REM 1) Social-Engineering Attacks
+REM 2) Website Attack Vectors
+REM 3) Credential Harvester Attack Method
+REM 2) Site Cloner
+REM ip da propria maquina
+REM pagina a ser clonada
